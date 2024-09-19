f51381500ced730fd12f3657d7277994fe53aa42
PavloVovk2002 <151703582+PavloVovk2002@users.noreply.github.com>
2024-09-18 22:15:24 -0500

    missing value in each column

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+import numpy as np
 import pandas as pd
+import random
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+df = pd.read_csv('Social-Media-Health-Impact/Main/Mental_Health_Survey_Feb_20_22.csv')
+for column in df.columns:
+  missing_values = df[column].isnull().sum()
+  print(f"Column '{column}' has {missing_values} missing value(s).")
+
+
+
