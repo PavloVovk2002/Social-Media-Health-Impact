55c31ed634fa5a55aa5ea642dedc09543b367927
PavloVovk2002 <pavlovovk1@gmail.com>
2024-09-25 17:09:39 -0500

    comment

--- Main.py
+++ Main.py
@@ -8,7 +8,6 @@ from sklearn.cluster import KMeans
 
 #data_df = pd.read_csv('/Users/pavlovovk/Documents/GitHub/Social-Media-Health-Impact/South_East_Asia_Social_Media_MentalHealth.csv')
 #data_df = pd.read_csv('/Users/gopivaghani/Documents/GitHub/Social-Media-Health-Impact/South_East_Asia_Social_Media_MentalHealth.csv')
-#data_df = pd.read_csv('/Users/gopivaghani/Documents)
 #data_df = pd.read_csv('/Users/amaankhan/Documents/GitHub/Social-Media-Health-Impact/South_East_Asia_Social_Media_MentalHealth.csv')
 
 #Clean
